a831e5cc5d275a097a70e3c441ec966a5addad73
Sergei Chipiga <sergei.chipiga@pipedrive.com>
2019-02-14 09:36:11 +0200

    Update project config

--- setup.py
+++ setup.py
@@ -7,23 +7,11 @@ Python project proto
 from setuptools import setup, find_packages
 
 setup(
-    name='python-project-proto',
-    version='0.0.1',
-    description='Simple example how to build python project with good style',
-    long_description=open('README.rst').read(),
-    author='Sergei Chipiga',
-    author_email='chipiga86@gmail.com',
-    license='MIT',
+    version="0.0.1",
     packages=find_packages(),
-    url='https://github.com/schipiga/ppp',
     install_requires=[
-        'requests==2.21.0',
-        'beautifulsoup4==4.7.1',
-        'PyYAML==3.13',
+        "requests==2.21.0",
+        "beautifulsoup4==4.7.1",
+        "PyYAML==3.13",
     ],
-    entry_points={
-        "console_scripts": [
-            "links = ppp.cli:main",
-        ],
-    },
 )
